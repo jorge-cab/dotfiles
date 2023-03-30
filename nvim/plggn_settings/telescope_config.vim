b2206e88b553a1490691b91d840966e30ed02f39
Jorge Cabiedes <jorgecabiedes99@hotmail.com>
2023-03-30 11:29:16 -0600

    Added current nord-theme config files

--- nvim/plggn_settings/telescope_config.vim
+++
@@ -1,4 +0,0 @@
-nnoremap ;f <cmd>Telescope find_files<cr>
-nnoremap ;g <cmd>Telescope live_grep<cr>
-nnoremap ;b <cmd>Telescope buffers<cr>
-nnoremap ;h <cmd>Telescope help_tags<cr>
--- nvim/plggn_settings/tree-sitter-config.lua
+++
@@ -1,8 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = true,
-    disable = {'css', 'vim'},
-  },
-  indent = { enable = true, disable = {} },
-}
