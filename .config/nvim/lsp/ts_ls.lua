71f0a424a60cfb6c46c6172e8ace4209e24896aa
Jorge Cabiedes <jorgecabiedes99@hotmail.com>
2025-12-15 16:43:29 -0800

    Configure nvim lsp formatting

--- .config/nvim/lsp/ts_ls.lua
+++ .config/nvim/lsp/ts_ls.lua
@@ -1,35 +1,34 @@
 return {
-  -- Command to start the language server
-  cmd = { 'typescript-language-server', '--stdio' },
+    -- Command to start the language server
+    cmd = { 'typescript-language-server', '--stdio' },
 
-  -- Filetypes that should attach to this server
-  filetypes = {
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx',
-    'javascript',
-    'javascriptreact',
-    'javascript.jsx',
-  },
-
-  -- How to detect the project root
-  -- Files that share a root_dir will share the same LSP instance
-  root_markers = {
-    { 'tsconfig.json', 'jsconfig.json' }, -- equal-priority group
-    'package.json',
-    '.git',
-  },
-
-  -- Optional: basic settings for the server
-  settings = {
-    typescript = {
-      format = {
-        semicolons = 'insert',
-      },
+    -- Filetypes that should attach to this server
+    filetypes = {
+        'typescript',
+        'typescriptreact',
+        'typescript.tsx',
+        'javascript',
+        'javascriptreact',
+        'javascript.jsx',
     },
-  },
 
-  -- Enable single-file support (no root markers found)
-  single_file_support = true,
+    -- How to detect the project root
+    -- Files that share a root_dir will share the same LSP instance
+    root_markers = {
+        { 'tsconfig.json', 'jsconfig.json' }, -- equal-priority group
+        'package.json',
+        '.git',
+    },
+
+    -- Optional: basic settings for the server
+    settings = {
+        typescript = {
+            format = {
+                semicolons = 'insert',
+            },
+        },
+    },
+
+    -- Enable single-file support (no root markers found)
+    single_file_support = true,
 }
-
--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -2,25 +2,38 @@ vim.lsp.enable('lua_ls')
 vim.lsp.enable('ts_ls')
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
-      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-      vim.keymap.set('i', '<C-Space>', function()
-        vim.lsp.completion.get()
-      end)
-    end
-  end,
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+        if not client then return end
+
+        -- Autocomplete
+        if client.supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+            vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+            vim.keymap.set('i', '<C-Space>', function()
+                vim.lsp.completion.get()
+            end)
+        end
+        -- LSP Formatting
+        if client.supports_method(vim.lsp.protocol.Methods.textDocument_formatting) then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                end,
+            })
+        end
+    end,
 })
 
 -- Diagnostics
 vim.diagnostic.config({
-  -- Use the default configuration
-  -- virtual_lines = true
-  -- Alternatively, customize specific options
-  virtual_lines = {
-    -- Only show virtual line diagnostics for the current cursor line
-    current_line = true,
-  },
+    -- Use the default configuration
+    -- virtual_lines = true
+    -- Alternatively, customize specific options
+    virtual_lines = {
+        -- Only show virtual line diagnostics for the current cursor line
+        current_line = true,
+    },
 })
