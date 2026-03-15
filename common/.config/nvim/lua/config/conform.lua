3e71fecc1b77852164fb1da79049ca84b7be787b
Jorge Cabiedes <jorgecabiedes99@hotmail.com>
2026-03-15 13:57:40 -0700

    Add C++ lsp

--- common/.config/nvim/lua/config/conform.lua
+++ common/.config/nvim/lua/config/conform.lua
@@ -2,11 +2,17 @@ require("conform").setup({
     formatters_by_ft = {
         javascript = { "prettier" },
         typescript = { "prettier" },
+        cpp = { "clang-format" },
+        c = { "clang-format" },
+        h = { "clang-format" },
     },
     formatters = {
         prettier = {
             -- This ensures prettier only runs if a config file is found in the project's CWD
             require_cwd = true,
+        },
+        ["clang-format"] = {
+            prepend_args = { "--style={IndentWidth: 4}" },
         },
     },
     format_on_save = {
